da1c11257f205a88f1063f86f0162fa89c79b8cf
stephanie lee <tuggles123@icloud.com>
2018-07-23 12:20:57 -0700

    trash

--- trash.js
+++ trash.js
@@ -1,2 +1,18 @@
-var trash = createSprite(200, 375, 317, 180)
-trash.setAnimation("trash2.png")
+var mainState = {
+    preload: function(){   
+        game.load.image('trash', 'media/grey2.png');
+    },
+    scale.mainState = 0.5; 
+    //creates player in middle of the game
+    this.player = game.add.sprite(70, 100, 'player');
+    this.score = 0;
+    //sets the gravtiy of the player
+     this.player.body.gravity.y = 600;
+
+     for(var i = 0; i < level.length; i++){
+     for(var j = 0; j < level[i].length; j++){
+     var wall = game.add.sprite(30+20*j, 30+20*i, 'wall');
+     this.walls.add(wall);
+     wall.body.immovable = true;
+        }
+    }
